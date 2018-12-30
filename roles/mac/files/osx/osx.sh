0ed59a9645fd85a65402dcab9e778837ff4bc401
Alex Andrews <alex@alexandrews.info>
2018-12-30 10:13:04 +0000

    Make key repeat faster

--- roles/mac/files/osx/osx.sh
+++ roles/mac/files/osx/osx.sh
@@ -88,8 +88,8 @@ echo "Disable press-and-hold for keys in favor of key repeat"
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
 echo "Set a blazingly fast keyboard repeat rate"
-defaults write NSGlobalDomain InitialKeyRepeat -int 20
 defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write NSGlobalDomain InitialKeyRepeat -int 10
 
 echo "Automatically illuminate built-in MacBook keyboard in low light"
 defaults write com.apple.BezelServices kDim -bool true
